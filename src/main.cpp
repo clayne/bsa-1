013edd4359f12ce4d3296e0db76f09118eb90df9
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2020-02-09 21:50:35 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 int main([[maybe_unused]] int a_argc, [[maybe_unused]] const char* a_argv[])
 {
 	bsa::archive archive(R"(E:\Games\SteamLibrary\steamapps\common\Skyrim Special Edition\Data\Skyrim - Patch.bsa)");
-	archive.parse();
 	//archive.dump();
 
 	return EXIT_SUCCESS;
